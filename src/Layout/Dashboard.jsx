eee34c0264f76243d1335a164a891bfcffdb1e7d
Pranto-Nondie <pranto.cs.25@gmail.com>
2023-06-08 22:43:47 +0600

    progress

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,58 +1,3 @@
-// import { NavLink, Outlet } from "react-router-dom";
-// import {  FaWallet,  FaHome, FaUtensils, } from 'react-icons/fa';
-// import useAdmin from "../hooks/UseAdmin";
-// import useInstructor from "../hooks/useInstructor";
-
-// const Dashboard = () => {
-
-
-//     const [isAdmin] = useAdmin();
-//     const [isInstructor] = useInstructor();
-
-//     return (
-//         <div className="drawer drawer-mobile ">
-//             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-//             <div className="drawer-content flex flex-col items-center justify-center">
-//                 <Outlet></Outlet>
-//                 <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-
-//             </div>
-//             <div className="drawer-side bg-[#D1A054]">
-//                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-//                 <ul className="menu p-4 w-80">
-
-//                     {
-//                         isAdmin ? <>
-//                             <li><NavLink to="/dashboard/adminHome"><FaHome></FaHome> Admin Home</NavLink></li>
-//                             <li><NavLink to="/dashboard/manageClasses"> <FaUtensils></FaUtensils> Manage Classes</NavLink></li>
-//                             <li><NavLink to="/dashboard/manageUsers"><FaWallet></FaWallet>Manage Users </NavLink></li>
-
-
-//                         </> :
-//                             isInstructor ? <>
-//                                 <li><NavLink to="/dashboard/instructorHome"><FaHome></FaHome> Instructor Home</NavLink></li>
-//                                 <li><NavLink to="/dashboard/addClass"> <FaUtensils></FaUtensils>Add  Class</NavLink></li>
-//                                 <li><NavLink to="/dashboard/myClasses"><FaWallet></FaWallet> My Classes</NavLink></li>
-
-//                             </> : <>
-//                                 <li><NavLink to="/dashboard/studentHome"><FaHome></FaHome> Student Home</NavLink></li>
-//                                 <li><NavLink to="/dashboard/selectedClasses"> <FaUtensils></FaUtensils>Selected Classes</NavLink></li>
-//                                 <li><NavLink to="/dashboard/enrolledClasses"><FaWallet></FaWallet> Enrolled Classes</NavLink></li>
-
-//                             </>
-
-//                     }
-//                     <div className="divider"></div>
-//                     <li><NavLink to="/"><FaHome></FaHome> Home</NavLink> </li>
-
-//                 </ul>
-
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default Dashboard;
 
 import { NavLink, Outlet } from "react-router-dom";
 import { FaWallet, FaHome, FaUtensils } from 'react-icons/fa';
@@ -64,74 +9,127 @@ const Dashboard = () => {
     const [isInstructor] = useInstructor();
 
     return (
-        <div className="dashboard-layout">
-            <div className="dashboard-sidebar">
-                <ul className="menu">
-                    {isAdmin && (
-                        <>
-                            <li>
-                                <NavLink to="/dashboard/adminHome">
-                                    <FaHome /> Admin Home
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/dashboard/manageClasses">
-                                    <FaUtensils /> Manage Classes
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/dashboard/manageUsers">
-                                    <FaWallet /> Manage Users
-                                </NavLink>
-                            </li>
-                        </>
-                    )}
-                    {isInstructor && (
-                        <>
-                            <li>
-                                <NavLink to="/dashboard/instructorHome">
-                                    <FaHome /> Instructor Home
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/dashboard/addClass">
-                                    <FaUtensils /> Add Class
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/dashboard/myClasses">
-                                    <FaWallet /> My Classes
-                                </NavLink>
-                            </li>
-                        </>
-                    )}
-                    {!isAdmin && !isInstructor && (
-                        <>
-                            <li>
-                                <NavLink to="/dashboard/studentHome">
-                                    <FaHome /> Student Home
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/dashboard/selectedClasses">
-                                    <FaUtensils /> Selected Classes
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/dashboard/enrolledClasses">
-                                    <FaWallet /> Enrolled Classes
-                                </NavLink>
-                            </li>
-                        </>
-                    )}
-                    <li>
-                        <NavLink to="/">
-                            <FaHome /> Home
-                        </NavLink>
-                    </li>
-                </ul>
+        <div className="flex h-screen ">
+            <div className="w-1/4 bg-gray-200 pt-5 ">
+                <div className="p-5">
+                    <ul className="space-y-10">
+                        {isAdmin && (
+                            <>
+                                <li>
+                                    <NavLink
+                                        to="/dashboard/adminHome"
+                                        activeClassName="text-blue-500"
+                                        className="flex items-center space-x-2"
+                                    >
+                                        <FaHome />
+                                        <span className="font-bold">Admin Home</span>
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink
+                                        to="/dashboard/manageClasses"
+                                        activeClassName="text-blue-500"
+                                        className="flex items-center space-x-2"
+                                    >
+                                        <FaUtensils />
+                                        <span className="font-bold">Manage Classes</span>
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink
+                                        to="/dashboard/manageUsers"
+                                        activeClassName="text-blue-500"
+                                        className="flex items-center space-x-2"
+                                    >
+                                        <FaWallet />
+                                        <span className="font-bold">Manage Users</span>
+                                    </NavLink>
+                                </li>
+                            </>
+                        )}
+                        {isInstructor && (
+                            <>
+                                <li>
+                                    <NavLink
+                                        to="/dashboard/instructorHome"
+                                        activeClassName="text-blue-500"
+                                        className="flex items-center space-x-2"
+                                    >
+                                        <FaHome />
+                                        <span className="font-bold">Instructor Home</span>
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink
+                                        to="/dashboard/addClass"
+                                        activeClassName="text-blue-500"
+                                        className="flex items-center space-x-2"
+                                    >
+                                        <FaUtensils />
+                                        <span className="font-bold">Add Class</span>
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink
+                                        to="/dashboard/myClasses"
+                                        activeClassName="text-blue-500"
+                                        className="flex items-center space-x-2"
+                                    >
+                                        <FaWallet />
+                                        <span className="font-bold">My Classes</span>
+                                    </NavLink>
+                                </li>
+                            </>
+                        )}
+                        {!isAdmin && !isInstructor && (
+                            <>
+                                <li>
+                                    <NavLink
+                                        to="/dashboard/studentHome"
+                                        activeClassName="text-blue-500"
+                                        className="flex items-center space-x-2"
+                                    >
+                                        <FaHome />
+                                        <span className="font-bold">Student Home</span>
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink
+                                        to="/dashboard/selectedClasses"
+                                        activeClassName="text-blue-500"
+                                        className="flex items-center space-x-2"
+                                    >
+                                        <FaUtensils />
+                                        <span className="font-bold">Selected Classes</span>
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink
+                                        to="/dashboard/enrolledClasses"
+                                        activeClassName="text-blue-500"
+                                        className="flex items-center space-x-2"
+                                    >
+                                        <FaWallet />
+                                        <span className="font-bold">Enrolled Classes</span>
+                                    </NavLink>
+                                </li>
+                            </>
+                        )}
+                        <li>
+                            <NavLink
+                                to="/"
+                                exact
+                                activeClassName="text-blue-500"
+                                className="flex items-center space-x-2"
+                            >
+                                <FaHome />
+                                <span className="font-bold">Home</span>
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div className="dashboard-content">
+            <div className="w-3/4 bg-white">
                 <Outlet />
             </div>
         </div>
