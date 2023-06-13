2f6c8d7cfb14d9c1b46d60264e208c3434237dd5
Pranto-Nondie <pranto.cs.25@gmail.com>
2023-06-13 18:01:51 +0600

    progres

--- src/Pages/Home/PopularInstructor/PopularInstructor.jsx
+++ src/Pages/Home/PopularInstructor/PopularInstructor.jsx
@@ -14,12 +14,12 @@ const PopularInstructor = () => {
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                 {instructors.slice(0, 6).map((instructor, index) => (
                     <div key={index} className="card w-96 bg-base-100 shadow-xl">
-                        <figure className="px-10 pt-10">
-                            <img src="https://media.photographycourse.net/wp-content/uploads/2022/06/08135516/person-taking-photographs-outdoors.jpg" alt={instructor.image} className="rounded-xl" />
+                        <figure className="px-10 pt-10 ">
+                            <img  src={instructor.image} alt={instructor.image} className="rounded-xl w-[80%] h-60 " />
                         </figure>
                         <div className="card-body items-center text-center">
-                            <h2 className="card-title">{instructor.instructor}</h2>
-                            <p>{instructor.description}</p>
+                            <h2 className="card-title">{instructor.instructorName}</h2>
+                            
                             <div className="card-actions">
                                 <button className="btn bg-emerald-200">View profile</button>
                             </div>
