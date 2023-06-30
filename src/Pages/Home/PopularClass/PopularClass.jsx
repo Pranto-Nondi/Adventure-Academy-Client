91bd9ce9f3dcf0520e32d70504678362e77a5d99
Pranto-Nondie <pranto.cs.25@gmail.com>
2023-07-01 03:10:57 +0600

    progress

--- src/Pages/Home/PopularClass/PopularClass.jsx
+++ src/Pages/Home/PopularClass/PopularClass.jsx
@@ -1,37 +1,5 @@
 
 
-// import React from "react";
-// import usePopularClass from "../../../hooks/usePopularClass";
-
-// const PopularClasses = () => {
-//     const [classes] = usePopularClass();
-
-//     return (
-//         <div className="container mx-auto my-10 px-2">
-//             <h2 className="text-3xl font-bold mb-6">Popular Classes</h2>
-//             <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-//                 {classes.slice(0, 6).map((classItem) => (
-//                     <div key={classItem._id} className="card bg-base-100 shadow-xl">
-//                         <figure>
-//                             <img className="w-full h-48 object-cover" src={classItem?.imgURL} alt={classItem.title} />
-//                         </figure>
-//                         <div className="p-4">
-//                             <h2 className="text-xl font-bold mb-2">{classItem.className}</h2>
-//                             <p className="text-lg mb-4">Price: {classItem.price}$</p>
-//                             <div className="flex justify-end">
-//                                 <button className="btn btn-primary">Add to Cart</button>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 ))}
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default PopularClasses;
-
-
 
 
 import React from "react";
